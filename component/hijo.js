d9bb763d1ef63c9822beefb7e0ff98881f6a6454
Matias <mmatiast@miumg.edu.gt>
2021-10-14 22:41:26 -0600

    vuex

--- component/hijo.js
+++ component/hijo.js
@@ -3,8 +3,17 @@ Vue.component('hijo',{
     `
     <div class="p-5 bg-success">
     <h4>componente hijo: {{numero}}</h4>
+    <h4>Nombre: {{nombre}}</h4>
     </div>
     `,
-    props:['numero']
+    props:['numero'],
+    data(){
+        return{
+            nombre: 'matias'
+        }
+    },
+    mounted(){
+        this.$emit('nombreHijo',this.nombre);
+    },
 
 })
